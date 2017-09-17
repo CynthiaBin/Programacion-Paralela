c4d4dcac0731072cd6e0c9142d61e6670beff7db
CynthiaBin <peter.t.r.u.c.h.a@gmail.com>
2017-09-16 20:03:40 -0500

    Archivo de prueba agregado

---
+++ practica_1/test.rb
@@ -0,0 +1,13 @@
+require_relative 'practica'
+
+puts caesars_crypt('hola Mundo233___')
+puts caesars_crypt('UBYNZHAQB')
+puts caesars_crypt('abcd')
+puts caesars_crypt('NOPQ')
+
+puts palindrome?('abcdcba')
+puts palindrome?('abcddcba')
+puts palindrome?('nope')
+
+puts sum_squares([1, 2, 3, 4])
+puts sum_squares((1..10))
