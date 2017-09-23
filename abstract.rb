f3ab7633dd81e9a21760183ac78604365124845e
CynthiaBin <peter.t.r.u.c.h.a@gmail.com>
2017-09-23 17:01:00 -0500

    Clase abstracta + migracion para test

--- abstract.rb
+++ abstract.rb
@@ -1,15 +1,23 @@
+require 'helper'
 
+DB = Sequel.connect(Helper.constructURL)
 
 class abstractBase
 
     def intialized(*data)
+        @alias = data[0]
+        @name = data[1]
+        @link =data[2]
     end
 
     def register
-
+        dataSet = DB[:table]
+        dataset.insert(alias: @alias, name: @name, link: @link)
     end
 
-    def by_id
+    def by_id(id)
+        dataSet = DB[:table]
+        dataset.select(alias: @alias, name: @name, link: @link)
     end
 
     def list_all
@@ -17,5 +25,7 @@ class abstractBase
 
     def by_name
     end
+
+    
 
 end
